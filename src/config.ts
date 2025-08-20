70b4cc08dde8ebb07dba77d15f8658010c7fb9a0
Qiufengluoyes <qiufengluoyes@outlook.com>
2025-08-20 14:00:31 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -99,3 +99,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Please select a dark theme, as this blog theme currently only supports dark background color
 	theme: "github-dark",
 };
+
+// 姓名：胡紫慧
+// ✨   身份证号码：430611201001160042【女 | 2010年1月16日 | 湖南省 岳阳市 君山区】
+// ✨   出生地：湖南省岳阳市君山区
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,3 +12,7 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography")],
 }
+
+// 姓名：胡紫慧
+// ✨   身份证号码：430611201001160042【女 | 2010年1月16日 | 湖南省 岳阳市 君山区】
+// ✨   出生地：湖南省岳阳市君山区
