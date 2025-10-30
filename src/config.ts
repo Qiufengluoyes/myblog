2be31508c9c01d89b1d7a353f468735b6662ad98
Qiufengluoyes <qiufengluoyes@outlook.com>
2025-10-30 23:26:44 +0800

    feat(NavBar): 图标更新

--- src/config.ts
+++ src/config.ts
@@ -48,11 +48,11 @@ export const siteConfig: SiteConfig = {
 
 export const navBarConfig: NavBarConfig = {
 	links: [
-		{ name: "主页", url: "/", icon: "fa6-solid:house" },
-		{ name: "归档", url: "/archive/", icon: "fa6-solid:box-archive" },
-		{ name: "挚友", url: "/friends/", icon: "fa6-solid:link" },
-		{ name: "追番", url: "/anime/", icon: "fa6-solid:tv" },
-		{ name: "关于", url: "/about/", icon: "fa6-solid:user" },
+		{ name: "主页", url: "/", icon: "fa7-solid:house" },
+		{ name: "归档", url: "/archive/", icon: "fa7-solid:box-archive" },
+		{ name: "挚友", url: "/friends/", icon: "fa7-solid:link" },
+		{ name: "追番", url: "/anime/", icon: "fa7-solid:display" },
+		{ name: "关于", url: "/about/", icon: "fa7-solid:user" },
 		{
 			name: "开往",
 			url: "https://www.travellings.cn/plain.html", // Internal links should not include the base path, as it is automatically added
---
+++ src/content/posts/Internet-meme.md
@@ -0,0 +1,13 @@
+---
+title: 个人对于网络烂梗的一些看法
+published: 2025-10-20
+description: '最近看了 B 站 UP 主「王小七Fire」的有关于网络烂梗的视频，结合现实的状况有感而发。'
+image: ''
+tags: ['网络烂梗','素养']
+category: '社会观察'
+draft: false 
+pinned: true
+---
+
+<iframe width="100%" height="468" src="//player.bilibili.com/player.html?isOutside=true&aid=115388859809848&bvid=BV1abWEzUEJe&cid=33152241017&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" autoplay="0" allowfullscreen="true"> </iframe>
+
