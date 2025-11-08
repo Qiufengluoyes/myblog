48a65853dcfd9a713e913dbd42b757d9fbef3575
Qiufengluoyes <qiufengluoyes@outlook.com>
2025-11-08 16:51:32 +0800

    fix(config): set theme color to fixed

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 	themeColor: {
 		hue: 285, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		// spring 160, summer 220, autumn 40, winter 290
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: true,
